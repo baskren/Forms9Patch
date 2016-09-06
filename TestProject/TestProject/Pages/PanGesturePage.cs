fab2acf10296b9fb447a281746403e8a6997fab5
Ben Askren <ben@buildcalc.com>
2016-09-06 14:12:02 -0400

    Updated PanGesturePage to add touch information in debug

--- TestProject/TestProject/Pages/PanGesturePage.cs
+++ TestProject/TestProject/Pages/PanGesturePage.cs
@@ -8,6 +8,9 @@ namespace TestProject
 		internal Xamarin.Forms.AbsoluteLayout absoluteLayout;
 		internal Xamarin.Forms.RelativeLayout relativeLayout;
 
+		void OnDown(object sender, FormsGestures.DownUpEventArgs e)
+		{
+		}
 
 		public PanGesturePage ()
 		{
@@ -20,63 +23,72 @@ namespace TestProject
 			};
 			button.Clicked += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON CLICKED!!!!");
 
-
+			/*
 			var buttonListener = new FormsGestures.Listener(button);
-			buttonListener.Down += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER DOWN"); // does not work with UIControl derived elements
+			buttonListener.Down += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON DOWN ["+e.Touches[0]+"]"); // does not work with UIControl derived elements
 
-			buttonListener.LongPressing += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER LONG PRESSING");
+			buttonListener.LongPressing += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON LONG PRESSING ["+e.Touches[0]+"]");
 			buttonListener.Panning += (sender, e) => {
+				System.Diagnostics.Debug.WriteLine("\tBUTTON PANNING ["+e.Touches[0]+"]");
 				button.TranslationX += e.DeltaDistance.X;
 				button.TranslationY += e.DeltaDistance.Y;
 			};
 			buttonListener.Pinching += (sender, e) =>  {
+				System.Diagnostics.Debug.WriteLine("\tBUTTON PINCHING ["+e.Touches[0]+"]");
 				button.Scale *= e.DeltaScale;
 			};
 			buttonListener.Rotating += (sender, e) => {
+				System.Diagnostics.Debug.WriteLine("\tBUTTON ROTATING ["+e.Touches[0]+"]");
 				button.Rotation += e.DeltaAngle;
 			};
 
-			buttonListener.Up += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER UP");		 // does not work with UIControl derived elements
-			buttonListener.Tapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER TAPPED");  // does not work with UIControl derived elements
-			buttonListener.DoubleTapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER DOUBLE TAPPED"); // does not work with UIControl derived elements
+			buttonListener.Up += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON UP ["+e.Touches[0]+"]");		 // does not work with UIControl derived elements
+			buttonListener.Tapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON TAPPED ["+e.Touches[0]+"]");  // does not work with UIControl derived elements
+			buttonListener.DoubleTapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON DOUBLE TAPPED ["+e.Touches[0]+"]"); // does not work with UIControl derived elements
 
-			buttonListener.LongPressed += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER LONG PRESSED");
-			buttonListener.Panned += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER PANNED");
-			buttonListener.Swiped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER SWIPED!!!");
+			buttonListener.LongPressed += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON LONG PRESSED ["+e.Touches[0]+"]");
+			buttonListener.Panned += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON PANNED TotalDistance=["+e.TotalDistance+"]");
+			buttonListener.Swiped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON SWIPED!!! Velocity=["+e.VelocityX+","+e.VelocityY+"]");
 
-			buttonListener.Pinched += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER PINCHED");
-			buttonListener.Rotated += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tLISTENER ROTATED");
-
+			buttonListener.Pinched += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON PINCHED TotalScale=["+e.TotalScale +"]");
+			buttonListener.Rotated += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBUTTON ROTATED TotalAngle=["+e.TotalAngle+"]");
+*/
 
 			var box = new BoxView {
 				BackgroundColor = Color.Green,
 			};
 			var boxListener = new FormsGestures.Listener (box);
-			boxListener.Down += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX DOWN"); 
 
-			boxListener.LongPressing += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX LONG PRESSING");
+			//boxListener.Down += OnDown;
+			boxListener.Down += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX DOWN ["+e.Touches[0]+"]"); 
+
+			boxListener.LongPressing += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX LONG PRESSING ["+e.Touches[0]+"]");
+
 			boxListener.Panning += (sender, e) => {
+				System.Diagnostics.Debug.WriteLine("\tBOX PANNING ["+e.Touches[0]+"]");
 				box.TranslationX += e.DeltaDistance.X;
 				box.TranslationY += e.DeltaDistance.Y;
 			};
+
 			boxListener.Pinching += (sender, e) =>  {
+				System.Diagnostics.Debug.WriteLine("\tBOX PINCHING ["+e.Touches[0]+"]");
 				box.Scale *= e.DeltaScale;
 			};
 			boxListener.Rotating += (sender, e) => {
+				System.Diagnostics.Debug.WriteLine("\tBOX ROTATING ["+e.Touches[0]+"]");
 				box.Rotation += e.DeltaAngle;
 			};
 
-			boxListener.Up += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX UP");		 
-			boxListener.Tapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX TAPPED");  
-			boxListener.DoubleTapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX DOUBLE TAPPED");
+			boxListener.Up += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX UP ["+e.Touches[0]+"]");		 
+			boxListener.Tapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX TAPPED ["+e.Touches[0]+"]");  
+			boxListener.DoubleTapped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX DOUBLE TAPPED ["+e.Touches[0]+"]");
 
-			boxListener.LongPressed += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX LONG PRESSED");
-			boxListener.Panned += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX PANNED");
-			boxListener.Swiped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX SWIPED!!!");
+			boxListener.LongPressed += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX LONG PRESSED ["+e.Touches[0]+"]");
+			boxListener.Panned += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX PANNED TotalDistance=["+e.TotalDistance+"]");
+			boxListener.Swiped += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX SWIPED!!! Velocity="+e.VelocityX+","+e.VelocityY+"]");
 
-			boxListener.Pinched += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX PINCHED");
-			boxListener.Rotated += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX ROTATED");
-
+			boxListener.Pinched += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX PINCHED TotalScale=["+e.TotalScale+"]");
+			boxListener.Rotated += (sender, e) => System.Diagnostics.Debug.WriteLine ("\tBOX ROTATED TotalAngle["+e.TotalAngle+"]");
 
 			relativeLayout = new Xamarin.Forms.RelativeLayout {
 				HorizontalOptions = LayoutOptions.FillAndExpand,
@@ -95,8 +107,8 @@ namespace TestProject
 			relativeLayout.Children.Add(box, 
 				xConstraint: 		Constraint.RelativeToParent ((parent) => { return parent.X + parent.Width/4;}),
 				yConstraint: 		Constraint.RelativeToParent ((parent) => { return parent.Y + parent.Height/4;}),
-				widthConstraint: 	Constraint.RelativeToParent ((parent) => { return parent.Width/8;}),
-				heightConstraint: 	Constraint.RelativeToParent ((parent) => { return parent.Height/10;})
+				widthConstraint: 	Constraint.RelativeToParent ((parent) => { return parent.Width/3;}),
+				heightConstraint: 	Constraint.RelativeToParent ((parent) => { return parent.Height/4;})
 			);
 
 			absoluteLayout = new Xamarin.Forms.AbsoluteLayout {
