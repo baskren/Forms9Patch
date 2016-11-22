8371bd8608615ccf7c4143b2b9602a9586985756
Ben Askren <ben@buildcalc.com>
2016-11-22 11:57:08 -0500

    oops!

--- Forms9Patch/Forms9Patch/Models/Settings.cs
+++ Forms9Patch/Forms9Patch/Models/Settings.cs
@@ -6,7 +6,7 @@ namespace Forms9Patch
 	/// <summary>
 	/// Forms9Patch Settings (for use by Forms9Patch PCL code).
 	/// </summary>
-	public static class Settings
+	public class Settings
 	{
 		/// <summary>
 		/// The shadow offset.
@@ -19,6 +19,16 @@ namespace Forms9Patch
 		public static double ShadowRadius = 3;
 
 		internal static TimeSpan MsUntilTapped = TimeSpan.FromMilliseconds(210);
+
+
+		public static double ListViewCellSwipePopupMenuWidthRequest = 250;
+		public static double ListViewCellSwipePopupMenuFontSize = 16;
+		public static Color ListViewCellSwipePopupMenuFontColor = Color.FromHex("0076FF");
+		public static Color ListViewCellSwipePopupMenuButtonColor = Color.White;
+		public static Color ListViewCellSwipePopupMenuButtonOutlineColor = Color.FromHex("CCC");
+		public static float ListViewCellSwipePopupMenuButtonOutlineWidth = 0;
+		public static float ListViewCellSwipePopupMenuButtonOutlineRadius = 6;
+		public static float ListViewCellSwipePopupMenuButtonSeparatorWidth = 0;
 	}
 }
 
