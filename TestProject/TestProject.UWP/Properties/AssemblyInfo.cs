fbe6c2ec0ff76ae22330e38c87e21564b7062dbb
DESKTOP-SO17ACK\ben <ben@buildcalc.com>
2017-12-15 08:08:58 -0500

    Start of renaming TestProject to Forms9PatchDemo

--- TestProject/TestProject.UWP/Properties/AssemblyInfo.cs
+++ TestProject/TestProject.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestProject.UWP")]
+[assembly: AssemblyTitle("Forms9PatchDemo.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestProject.UWP")]
+[assembly: AssemblyCompany("42nd Parallel")]
+[assembly: AssemblyProduct("Forms9PatchDemo.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
