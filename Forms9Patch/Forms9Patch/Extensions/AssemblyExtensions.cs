f38718aae9a7e1e652cd47ab8ca64f16fe30f686
Ben Askren <ben@buildcalc.com>
2017-12-14 21:40:19 -0500

    Fixed: Broke iOS and Android ability to get embedded resources based on embeddedResourceId

--- Forms9Patch/Forms9Patch/Extensions/AssemblyExtensions.cs
+++ Forms9Patch/Forms9Patch/Extensions/AssemblyExtensions.cs
@@ -35,24 +35,27 @@ namespace Forms9Patch
                             return asm;
                 }
             }
-            for (int i = resourcePath.Count - 1; i < 0; i--)
+            if (assembly == null)
             {
-                var asmName = string.Join(".", resourcePath.GetRange(0, i));
-                if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+                for (int i = resourcePath.Count - 1; i < 0; i--)
                 {
-                    assembly = PCL.Utils.ReflectionExtensions.GetAssemblyByName(asmName);
-                    if (assembly != null)
-                        return assembly;
-                }
-                else if (Device.RuntimePlatform == Device.UWP)
-                {
-                    foreach (var asm in Settings.IncludedAssemblies)
-                        if (asm?.GetName().Name == asmName)
-                            return asm;
-                }
+                    var asmName = string.Join(".", resourcePath.GetRange(0, i));
+                    if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+                    {
+                        assembly = PCL.Utils.ReflectionExtensions.GetAssemblyByName(asmName);
+                        if (assembly != null)
+                            return assembly;
+                    }
+                    else if (Device.RuntimePlatform == Device.UWP)
+                    {
+                        foreach (var asm in Settings.IncludedAssemblies)
+                            if (asm?.GetName().Name == asmName)
+                                return asm;
+                    }
 
+                }
             }
-            return null;
+            return assembly;
         }
     }
 }
