91c3cb4972e1bf7906c90cb57acc6ce796d4dada
DESKTOP-SO17ACK\ben <ben@buildcalc.com>
2017-12-24 11:34:44 -0500

    Added missing embedded resource images to NetStandard project (used by demo app) NetStandard.UWP crash when trying to write to EmbeddedResourceCache

--- Forms9Patch/Forms9Patch.UWP/Models/Settings.cs
+++ Forms9Patch/Forms9Patch.UWP/Models/Settings.cs
@@ -74,7 +74,8 @@ namespace Forms9Patch.UWP
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     }
                     //System.Diagnostics.Debug.WriteLine("[" + PCL.Utils.ReflectionExtensions.CallerMemberName() + "] 2");
-                    //FormsGestures.Droid.Settings.Init();
+                    FormsGestures.UWP.Settings.Init();
+
                 }
             }
             get
