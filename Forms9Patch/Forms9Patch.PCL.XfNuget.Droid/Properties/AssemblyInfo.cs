93f84b3c60fd1ba516bde464295feddca65eeb8f
DESKTOP-SO17ACK\ben <ben@buildcalc.com>
2018-01-22 15:40:21 -0500

    Mid-refactor

---
+++ Forms9Patch/Forms9Patch.PCL.XfNuget.Droid/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+using Forms9Patch;
+
+[assembly: AssemblyTitle("Forms9Patch.Droid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("42nd Parallel")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("42nd Parallel, 2015")]
+[assembly: AssemblyTrademark("42nd Parallel")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.0")]
+
+//[assembly: ResolutionGroupName("Forms9Patch")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
+//[assembly: ExportRenderer(typeof(Forms9Patch.Image), typeof(Forms9Patch.Droid.ImageRenderer))]
+//[assembly: ExportRenderer(typeof(Forms9Patch.BubbleLayout), typeof(Forms9Patch.Droid.BubbleLayoutRenderer))]
+//[assembly: ExportRenderer(typeof(Forms9Patch.ManualLayout), typeof(Forms9Patch.Droid.ManualLayoutRenderer))]
---
+++ FormsGestures/FormsGestures.NetStandard.XfSource/Properties/AdditionalAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("FormsGestures.Droid")]
+[assembly: InternalsVisibleTo("FormsGestures.iOS")]
+[assembly: InternalsVisibleTo("FormsGestures.UWP")]
+
