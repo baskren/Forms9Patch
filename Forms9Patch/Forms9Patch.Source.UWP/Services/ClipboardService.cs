5f47e836fc6cca4661807f2475e79c75a55e94cc
DESKTOP-SO17ACK\ben <ben@buildcalc.com>
2018-02-21 15:09:27 -0500

    Working: UWP Clipboard

--- Forms9Patch/Forms9Patch.Source.UWP/Services/ClipboardService.cs
+++ Forms9Patch/Forms9Patch.Source.UWP/Services/ClipboardService.cs
@@ -1,30 +1,64 @@
 using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.UWP;
-using Windows.ApplicationModel.DataTransfer;
+//using Windows.ApplicationModel.DataTransfer;
 
 [assembly: Dependency(typeof(Forms9Patch.UWP.ClipboardService))]
 namespace Forms9Patch.UWP
 {
     public class ClipboardService : Forms9Patch.IClipboardService
     {
-        public DataPackage DataPackage = new DataPackage();
+        public Windows.ApplicationModel.DataTransfer.DataPackage DataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
 
-        public string Value
+        public ClipboardData Data
         {
             get
             {
-                DataPackageView dataPackageView = Clipboard.GetContent();
-                if (dataPackageView.Contains(StandardDataFormats.Text))
-                {
-
-                }
+                var result = new ClipboardData();
+                var dataPackageView = Windows.ApplicationModel.DataTransfer.Clipboard.GetContent();
+                if (dataPackageView.Contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.Text))
+                    result.PlainText = dataPackageView.GetText();
+                if (dataPackageView.Contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.Html))
+                    result.HtmlText = dataPackageView.GetHtmlText();
+                return result;
             }
             set
             {
-                //UIPasteboard.General.String = value;
-
+                if (value == null)
+                    return;
+                var dataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
+                if (!string.IsNullOrEmpty(value.PlainText))
+                    dataPackage.SetText(value.PlainText);
+                if (!string.IsNullOrEmpty(value.HtmlText))
+                    dataPackage.SetHtmlFormat(value.HtmlText);
+                Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage);
             }
         }
+    }
+
+    static class DataPackageViewExtensions
+    {
+        public static string GetText(this Windows.ApplicationModel.DataTransfer.DataPackageView dpv)
+        {
+            var task = Task.Run(async () =>
+            {
+                var result = await dpv.GetTextAsync();
+                return result;
+            });
+            return task.Result;
+        }
+
+        public static string GetHtmlText(this Windows.ApplicationModel.DataTransfer.DataPackageView dpv)
+        {
+            var task = Task.Run(async () =>
+            {
+                var result = await dpv.GetHtmlFormatAsync();
+                return result;
+            });
+            return task.Result;
+        }
+
+
     }
 }
