0444fbfd7ff5a6f0f941881e8436cc6855b82ce5
Ben Askren <ben@buildcalc.com>
2018-03-01 13:36:33 -0500

    Swipe menu copy/paste using Clipboard is working

--- Forms9Patch/Forms9Patch.Source/Elements/ListView/Interfaces/ICellSwipeMenus.cs
+++ Forms9Patch/Forms9Patch.Source/Elements/ListView/Interfaces/ICellSwipeMenus.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 
 namespace Forms9Patch
 {
-	/// <summary>
-	/// Interface to implement Swipe Menus in content views for Forms9Patch ListView cells
-	/// </summary>
-	public interface ICellSwipeMenus
-	{
-		/// <summary>
-		/// Gets the start swipe menu items.
-		/// </summary>
-		/// <value>The start swipe menu.</value>
-		List<SwipeMenuItem> StartSwipeMenu { get; }
+    /// <summary>
+    /// Interface to implement Swipe Menus in content views for Forms9Patch ListView cells
+    /// </summary>
+    public interface ICellSwipeMenus
+    {
+        /// <summary>
+        /// Gets the start swipe menu items.
+        /// </summary>
+        /// <value>The start swipe menu.</value>
+        List<SwipeMenuItem> StartSwipeMenu { get; }
 
-		/// <summary>
-		/// Gets the end swipe menu items.
-		/// </summary>
-		/// <value>The end swipe menu.</value>
-		List<SwipeMenuItem> EndSwipeMenu { get; }
+        /// <summary>
+        /// Gets the end swipe menu items.
+        /// </summary>
+        /// <value>The end swipe menu.</value>
+        List<SwipeMenuItem> EndSwipeMenu { get; }
 
-		/// <summary>
-		/// Ons the swipe menu item button tapped.
-		/// </summary>
-		/// <param name="sender">Sender.</param>
-		/// <param name="args">Arguments.</param>
-		void OnSwipeMenuItemButtonTapped(object sender, SwipeMenuItemTappedArgs args);
-	}
+        /// <summary>
+        /// Ons the swipe menu item button tapped.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="args">Arguments.</param>
+        void OnSwipeMenuItemButtonTapped(object sender, SwipeMenuItemTappedArgs args);
+
+    }
 }
