b07c9f2b44cd6d3671010a62ed5c8a6666442d0c
Ben Askren <ben@buildcalc.com>
2018-04-12 09:55:34 -0400

    Fixed: - crash on pre-API 18 versions of Android when rendering SinglePicker

---
+++ Forms9Patch/Forms9Patch.Source.Droid/Services/OsInfoService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Forms9Patch.Droid.OsInfoService))]
+namespace Forms9Patch.Droid
+{
+    class OsInfoService : IOsInformationService
+    {
+        public Version Version
+        {
+            get
+            {
+                var revision = Android.OS.Build.VERSION.Release;
+                return new Version(revision);
+            }
+        }
+    }
+}
---
+++ Forms9Patch/Forms9Patch.Source.iOS/Services/OsInfoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using UIKit;
+
+[assembly: Dependency(typeof(Forms9Patch.iOS.OsInfoService))]
+namespace Forms9Patch.iOS
+{
+    class OsInfoService : IOsInformationService
+    {
+        public Version Version
+        {
+            get
+            {
+                var revision = UIDevice.CurrentDevice.SystemVersion;
+                return new Version(revision);
+            }
+        }
+    }
+}
