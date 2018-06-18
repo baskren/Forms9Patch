b5cde8330e874f4d6b417802a6e2ffec03e5a625
Ben Askren <ben@buildcalc.com>
2018-06-18 06:48:38 -0400

    Added UWP to KeyboardService.Height;  needs to be tested and debugged.

--- Forms9Patch/Forms9Patch.Source.UWP/Services/KeyboardService.cs
+++ Forms9Patch/Forms9Patch.Source.UWP/Services/KeyboardService.cs
@@ -6,12 +6,13 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Devices.Input;
+using Windows.Graphics.Display;
 using Windows.UI.ViewManagement;
 
 [assembly: Xamarin.Forms.Dependency(typeof(Forms9Patch.UWP.KeyboardService))]
 namespace Forms9Patch.UWP
 {
-    class KeyboardService : IKeyboardService
+    class KeyboardService : IKeyboardService, IDisposable
     {
         public bool IsHardwareKeyboardActive
         {
@@ -22,6 +23,8 @@ namespace Forms9Patch.UWP
             }
         }
 
+        Windows.Graphics.Display.DisplayInformation _displayInformation;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="T:Forms9Patch.iOS.KeyboardService"/> class.
         /// </summary>
@@ -31,16 +34,26 @@ namespace Forms9Patch.UWP
             InputPane.GetForCurrentView().Showing += KeyboardService_Showing;
         }
 
+        private void OnOrienationChanged(DisplayInformation sender, object args)
+        {
+            Height = InputPane.GetForCurrentView().OccludedRect.Height;
+        }
+
         private void KeyboardService_Showing(InputPane sender, InputPaneVisibilityEventArgs args)
         {
             Forms9Patch.KeyboardService.OnVisiblityChange(KeyboardVisibilityChange.Shown);
+            Height = InputPane.GetForCurrentView().OccludedRect.Height;
+            _displayInformation = Windows.Graphics.Display.DisplayInformation.GetForCurrentView();
+            _displayInformation.OrientationChanged += OnOrienationChanged;
         }
 
         private void KeyboardService_Hiding(InputPane sender, InputPaneVisibilityEventArgs args)
         {
             Forms9Patch.KeyboardService.OnVisiblityChange(KeyboardVisibilityChange.Hidden);
+            Height = InputPane.GetForCurrentView().OccludedRect.Height;
+            if (_displayInformation!=null)
+                _displayInformation.OrientationChanged -= OnOrienationChanged;
         }
-
 
         /// <summary>
         /// Hide this instance.
@@ -58,5 +71,57 @@ namespace Forms9Patch.UWP
             }
         }
 
+        double _height;
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (_height!=value)
+                {
+                    _height = value;
+                    Forms9Patch.KeyboardService.OnHeightChanged(_height);
+                }
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (_displayInformation!=null)
+                    {
+                        _displayInformation.OrientationChanged -= OnOrienationChanged;
+                        _displayInformation = null;
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~KeyboardService() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
