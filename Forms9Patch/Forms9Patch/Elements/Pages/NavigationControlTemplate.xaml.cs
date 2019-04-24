1201db6ba8405ddf65cfe23a0af7d3551ab85089
Ben Askren <ben@buildcalc.com>
2019-04-24 13:35:19 -0400

    Start of really big refactor to get rid of Xamarin.Forms source code references;

---
+++ Forms9Patch/Forms9Patch/Elements/Pages/NavigationControlTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Forms9Patch.Elements.Pages
+{
+    public partial class NavigationControlTemplate : ContentPage
+    {
+        public NavigationControlTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Forms9Patch/Forms9Patch/Elements/Pages/NavigationPage.cs
@@ -0,0 +1,10 @@
+using System;
+using Xamarin.Forms;
+
+namespace Forms9Patch
+{
+    public class NavigationPage : Xamarin.Forms.NavigationPage
+    {
+
+    }
+}
