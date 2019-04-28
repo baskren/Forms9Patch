ed7184b1b720c6f0c1106b4f0ece8fdb004cbd16
Ben Askren <ben@buildcalc.com>
2019-04-28 14:07:51 -0400

    Added ScrollView extensions;

---
+++ Forms9Patch/Forms9Patch/Extensions/ScrollViewExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Forms9Patch
+{
+    public static class ScrollViewExtensions
+    {
+        public static async Task ScrollToBottomAync(this Xamarin.Forms.ScrollView scrollView, bool animated = true)
+            => await scrollView.ScrollToAsync(Math.Max(0, scrollView.ScrollY), Math.Max(0, scrollView.Content.Height - scrollView.Height), animated);
+
+        public static async Task ScrollToRightAsync(this Xamarin.Forms.ScrollView scrollView, bool animated = true)
+            => await scrollView.ScrollToAsync(Math.Max(0, scrollView.Content.Width - scrollView.Width), Math.Max(0, scrollView.ScrollY), animated);
+
+        public static async Task ScrollToTopAsync(this Xamarin.Forms.ScrollView scrollView, bool animated = true)
+            => await scrollView.ScrollToAsync(Math.Max(0, scrollView.ScrollY), 0, animated);
+
+        public static async Task ScrollToLeftAsync(this Xamarin.Forms.ScrollView scrollView, bool animated = true)
+            => await scrollView.ScrollToAsync(0, Math.Max(0, scrollView.ScrollY), animated);
+
+        public static async Task ScrollToEndAsync(this Xamarin.Forms.ScrollView scrollView, bool animated = true)
+            => await scrollView.ScrollToAsync(Math.Max(0, scrollView.Content.Width - scrollView.Width), Math.Max(0, scrollView.Content.Height - scrollView.Height), animated);
+
+        public static async Task ScrollToStartAsync(this Xamarin.Forms.ScrollView scrollView, bool animated = true)
+            => await scrollView.ScrollToAsync(0, 0, animated);
+    }
+}
