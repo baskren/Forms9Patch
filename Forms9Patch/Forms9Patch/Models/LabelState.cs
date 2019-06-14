5d62273ecb41ff2de13c0380ab448e828e5cca78
Ben Askren <ben@buildcalc.com>
2019-06-14 08:40:00 -0400

    Part1: Address VS 2019 messages

---
+++ Forms9Patch/Forms9Patch/Models/LabelState.cs
@@ -0,0 +1,55 @@
+﻿using Xamarin.Forms;
+
+namespace Forms9Patch.UWP
+{
+    class LabelState
+    {
+        public TextAlignment HorizontalTextAlignment;
+        public TextAlignment VerticalTextAlignment;
+        public string FontFamily;
+        public double FontSize = -1;
+        public FontAttributes FontAttributes;
+        public string Text;
+        public LineBreakMode LineBreakMode;
+
+        public string HtmlText;
+        public AutoFit AutoFit = AutoFit.None;
+        public int Lines;
+        public double SynchronizedFontSize = -1;
+        public double FittedFontSize=-1;
+
+
+        public LabelState() { }
+
+        public LabelState(Label label)
+        {
+            HorizontalTextAlignment = label.HorizontalTextAlignment;
+            VerticalTextAlignment = label.VerticalTextAlignment;
+            FontFamily = label.FontFamily;
+            FontSize = label.FontSize;
+            FontAttributes = label.FontAttributes;
+            Text = label.Text;
+            HtmlText = label.HtmlText;
+            AutoFit = label.AutoFit;
+            Lines = label.Lines;
+
+            FittedFontSize = label.FittedFontSize;
+            SynchronizedFontSize = label.SynchronizedFontSize;
+        }
+
+        public bool HasChanged(Label label)
+        {
+            return FontFamily != label.FontFamily
+                || FontSize != label.FontSize
+                || FontAttributes != label.FontAttributes
+                || Text != label.Text
+                || HtmlText != label.HtmlText
+                || AutoFit != label.AutoFit
+                || Lines != label.Lines
+                || SynchronizedFontSize != label.SynchronizedFontSize;
+        }
+
+    }
+
+
+}
