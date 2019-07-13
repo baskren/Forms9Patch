c8ee017eda2b871a4fe526d9297f9b8c7b13a79a
Ben Askren <ben@buildcalc.com>
2019-07-13 17:55:02 -0400

    Probably doesn't do anything

--- Forms9Patch/Forms9Patch.iOS/Services/WebViewExtensionsService.cs
+++ Forms9Patch/Forms9Patch.iOS/Services/WebViewExtensionsService.cs
@@ -6,44 +6,48 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: Dependency(typeof(Forms9Patch.iOS.WebViewExtensionsService))]
 namespace Forms9Patch.iOS
 {
-	/// <summary>
-	/// Web view extensions service.
-	/// </summary>
-	public class WebViewExtensionsService : IWebViewExtensionService
-	{
-		/// <summary>
-		/// Print the specified viewToPrint and jobName.
-		/// </summary>
-		/// <param name="viewToPrint">View to print.</param>
-		/// <param name="jobName">Job name.</param>
-		public void Print(WebView viewToPrint, string jobName)
-		{
-			var appleViewToPrint = Platform.CreateRenderer(viewToPrint).NativeView;
+    /// <summary>
+    /// Web view extensions service.
+    /// </summary>
+    public class WebViewExtensionsService : IWebViewExtensionService
+    {
+        /// <summary>
+        /// Print the specified viewToPrint and jobName.
+        /// </summary>
+        /// <param name="viewToPrint">View to print.</param>
+        /// <param name="jobName">Job name.</param>
+        public void Print(WebView viewToPrint, string jobName)
+        {
+            var appleViewToPrint = Platform.CreateRenderer(viewToPrint).NativeView;
 
-			var printInfo = UIPrintInfo.PrintInfo;
+            var printInfo = UIPrintInfo.PrintInfo;
 
-			printInfo.OutputType = UIPrintInfoOutputType.General;
-			printInfo.JobName = jobName;
-			printInfo.Orientation = UIPrintInfoOrientation.Portrait;
-			printInfo.Duplex = UIPrintInfoDuplex.None;
+            printInfo.OutputType = UIPrintInfoOutputType.General;
+            printInfo.JobName = jobName;
+            printInfo.Orientation = UIPrintInfoOrientation.Portrait;
+            printInfo.Duplex = UIPrintInfoDuplex.None;
 
-			var printController = UIPrintInteractionController.SharedPrintController;
+            var printController = UIPrintInteractionController.SharedPrintController;
 
-			printController.PrintInfo = printInfo;
-			printController.ShowsPageRange = true;
-			printController.PrintFormatter = appleViewToPrint.ViewPrintFormatter;
+            printController.PrintInfo = printInfo;
+            printController.ShowsPageRange = true;
+            printController.PrintFormatter = appleViewToPrint.ViewPrintFormatter;
 
-			printController.Present(true, (printInteractionController, completed, error) => { });
-		}
+            printController.Present(true, (printInteractionController, completed, error) =>
+            {
+                System.Diagnostics.Debug.WriteLine(GetType() + "." + P42.Utils.ReflectionExtensions.CallerMemberName() + ": PRESENTED completed[" + completed + "] error[" + error + "]");
+                printInteractionController.PrintInfo = printInfo;
+            });
+        }
 
-		/// <summary>
-		/// Cans the print.
-		/// </summary>
-		/// <returns><c>true</c>, if print was caned, <c>false</c> otherwise.</returns>
-		public bool CanPrint()
-		{
-			return true;
-		}
+        /// <summary>
+        /// Cans the print.
+        /// </summary>
+        /// <returns><c>true</c>, if print was caned, <c>false</c> otherwise.</returns>
+        public bool CanPrint()
+        {
+            return true;
+        }
 
-	}	
+    }
 }
