8fa64efe6fc30a357780d215dd6dcdb83ae8e49e
Ben Askren <ben@buildcalc.com>
2019-08-13 09:47:45 -0700

    Fixed: Crash on pre-M versions of Android;

--- Forms9Patch/Forms9Patch.Droid/Extensions/TextExtensions.cs
+++ Forms9Patch/Forms9Patch.Droid/Extensions/TextExtensions.cs
@@ -8,19 +8,27 @@ namespace Forms9Patch.Droid
         public static Android.Text.StaticLayout StaticLayout(ICharSequence source, Android.Text.TextPaint paint, int width, Android.Text.Layout.Alignment align, float spacingmult, float spacingadd, bool includepad)
         {
             //P42.Utils.Debug.Message(source.ToString(), "ENTER");
-            var builder = Android.Text.StaticLayout.Builder.Obtain(source, 0, source.Length(), paint, width).SetAlignment(align).SetLineSpacing(spacingadd, spacingmult).SetIncludePad(includepad);
-            var layout = builder.Build();
-            //P42.Utils.Debug.Message(source.ToString(), "EXIT");
-            return layout;
+            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.M)
+            {
+                var builder = Android.Text.StaticLayout.Builder.Obtain(source, 0, source.Length(), paint, width).SetAlignment(align).SetLineSpacing(spacingadd, spacingmult).SetIncludePad(includepad);
+                var layout = builder.Build();
+                //P42.Utils.Debug.Message(source.ToString(), "EXIT");
+                return layout;
+            }
+            return new Android.Text.StaticLayout(source, paint, width, align, spacingmult, spacingadd, includepad);
         }
 
         public static Android.Text.StaticLayout StaticLayout(string source, Android.Text.TextPaint paint, int width, Android.Text.Layout.Alignment align, float spacingmult, float spacingadd, bool includepad)
         {
             //P42.Utils.Debug.Message(source, "ENTER");
-            var builder = Android.Text.StaticLayout.Builder.Obtain(source, 0, source.Length, paint, width).SetAlignment(align).SetLineSpacing(spacingadd, spacingmult).SetIncludePad(includepad);
-            var layout = builder.Build();
-            //P42.Utils.Debug.Message(source, "EXIT");
-            return layout;
+            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.M)
+            {
+                var builder = Android.Text.StaticLayout.Builder.Obtain(source, 0, source.Length, paint, width).SetAlignment(align).SetLineSpacing(spacingadd, spacingmult).SetIncludePad(includepad);
+                var layout = builder.Build();
+                //P42.Utils.Debug.Message(source, "EXIT");
+                return layout;
+            }
+            return new Android.Text.StaticLayout(source, paint, width, align, spacingmult, spacingadd, includepad);
         }
 
 
