c35702f4858dff3e9dc540bd61fd2e7329f1d4b5
Ben Askren <ben@buildcalc.com>
2019-10-10 13:41:36 -0400

    Huh?

---
+++ Forms9Patch/Forms9Patch.Droid/Popup/Popup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Forms9Patch.Elements.Popups.Core;
+using Xamarin.Forms;
+
+namespace Forms9Patch
+{
+    public static class Popup
+    {
+        public static bool SendBackPressed(Action onBackPressedHandler=null)
+        {
+            if (PopupNavigation.Instance.PopupStack.LastOrDefault() is PopupPage lastPage)
+            {
+                if (!(lastPage.DisappearingTransactionTask != null || lastPage.SendBackButtonPressed()))
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await PopupNavigation.Instance.PopAsync();
+                    });
+                }
+                return true;
+            }
+            onBackPressedHandler?.Invoke();
+            return false;
+        }
+    }
+}
+
