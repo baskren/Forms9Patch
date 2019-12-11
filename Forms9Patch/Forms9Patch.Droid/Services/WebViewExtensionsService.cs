c8d15925c5f94b1072e04294c61e617a48faa730
Ben Askren <ben@buildcalc.com>
2019-12-11 14:04:31 -0500

    Fixed: - Android WebView print not working;

--- Forms9Patch/Forms9Patch.Droid/Services/WebViewExtensionsService.cs
+++ Forms9Patch/Forms9Patch.Droid/Services/WebViewExtensionsService.cs
@@ -13,43 +13,49 @@ namespace Forms9Patch.Droid
             //var droidViewToPrint = Platform.CreateRenderer(viewToPrint).ViewGroup.GetChildAt(0) as Android.Webkit.WebView;
 
             //if (Platform.CreateRenderer(viewToPrint).View is Android.Webkit.WebView droidViewToPrint)
-            if (Platform.CreateRendererWithContext(viewToPrint, Settings.Context).View is Android.Webkit.WebView droidViewToPrint)
+            if (Platform.CreateRendererWithContext(viewToPrint, Settings.Context) is IVisualElementRenderer renderer)
             {
-                // Only valid for API 19+
-                var version = Android.OS.Build.VERSION.SdkInt;
-
-                if (version >= Android.OS.BuildVersionCodes.Kitkat)
+                Android.Webkit.WebView droidWebView = renderer.View as Android.Webkit.WebView;
+                if (droidWebView == null && renderer.View is Xamarin.Forms.Platform.Android.WebViewRenderer xfWebViewRenderer)
+                    droidWebView = xfWebViewRenderer.Control;
+                if (droidWebView != null)
                 {
-                    var printMgr = (PrintManager)Settings.Context.GetSystemService(Context.PrintService);
-                    printMgr.Print(jobName, droidViewToPrint.CreatePrintDocumentAdapter(jobName), null);
+                    // Only valid for API 19+
+                    var version = Android.OS.Build.VERSION.SdkInt;
 
-                    /*
-					var document = new Android.Graphics.Pdf.PdfDocument();
-					var page = document.StartPage(new Android.Graphics.Pdf.PdfDocument.PageInfo.Builder(360, 720, 1).Create());
+                    if (version >= Android.OS.BuildVersionCodes.Kitkat)
+                    {
+                        jobName = jobName ?? Forms9Patch.ApplicationInfoService.Name;
+                        var printMgr = (PrintManager)Settings.Context.GetSystemService(Context.PrintService);
+                        printMgr.Print(jobName, droidWebView.CreatePrintDocumentAdapter(jobName), null);
 
-					droidViewToPrint.Draw(page.Canvas);
-					document.FinishPage(page);
+                        /*
+                        var document = new Android.Graphics.Pdf.PdfDocument();
+                        var page = document.StartPage(new Android.Graphics.Pdf.PdfDocument.PageInfo.Builder(360, 720, 1).Create());
 
-					Stream memoryStream = new MemoryStream();
-					//var os = new ByteArrayOutputStream();
-					var fos = new Java.IO.FileOutputStream("/data/data/com.awc.hgts_areas/files" + "/" + "TEST" + ".pdf", false);
-					try
-					{
-						document.WriteTo(memoryStream);
-						fos.Write(((MemoryStream)memoryStream).ToArray(), 0, (int)memoryStream.Length);
-						fos.Close();
-						//var file = FileSystem.Current.LocalStorage.GetFile("TEST" + ".pdf");
-						//_onComplete?.Invoke(file);
-					}
-					catch
-					{
-						System.Diagnostics.Debug.WriteLine("{0}[{1}] FAILED TO WRITE", P42.Utils.ReflectionExtensions.CallerString(), GetType());
-						//_onComplete?.Invoke(null);
-					}
-					*/
+                        droidViewToPrint.Draw(page.Canvas);
+                        document.FinishPage(page);
+
+                        Stream memoryStream = new MemoryStream();
+                        //var os = new ByteArrayOutputStream();
+                        var fos = new Java.IO.FileOutputStream("/data/data/com.awc.hgts_areas/files" + "/" + "TEST" + ".pdf", false);
+                        try
+                        {
+                            document.WriteTo(memoryStream);
+                            fos.Write(((MemoryStream)memoryStream).ToArray(), 0, (int)memoryStream.Length);
+                            fos.Close();
+                            //var file = FileSystem.Current.LocalStorage.GetFile("TEST" + ".pdf");
+                            //_onComplete?.Invoke(file);
+                        }
+                        catch
+                        {
+                            System.Diagnostics.Debug.WriteLine("{0}[{1}] FAILED TO WRITE", P42.Utils.ReflectionExtensions.CallerString(), GetType());
+                            //_onComplete?.Invoke(null);
+                        }
+                        */
+                    }
                 }
             }
-
 
         }
 
