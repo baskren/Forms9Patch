1c0f19eb58981c6f4db4ab4b70684d3226a41b6b
Benjamin Askren <benjamin.askren@toyota.com>
2020-02-07 12:09:15 -0500

    Added EntryNoUnderlineEffect

---
+++ Forms9Patch/Forms9Patch.Droid/Effects/EntryNoUnderline.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(Forms9Patch.Droid.EntryNoUnderlineEffect), "EntryNoUnderlineEffect")]
+namespace Forms9Patch.Droid
+{
+    public class EntryNoUnderlineEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Control is Xamarin.Forms.Platform.Android.FormsEditText control)
+            {
+                control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+                control.SetPadding(control.PaddingLeft, 0, control.PaddingRight, 0);
+            }
+        }
+
+        protected override void OnDetached() { }
+
+
+
+    }
+
+}
