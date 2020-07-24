0483d6efecf99361969c6d5f1fb1407227e57537
Ben Askren <ben@buildcalc.com>
2020-07-24 14:07:38 -0400

    v 2.4.0 release

--- Forms9Patch/Forms9Patch.UWP/Printing/PrintService.cs
+++ Forms9Patch/Forms9Patch.UWP/Printing/PrintService.cs
@@ -35,17 +35,51 @@ namespace Forms9Patch.UWP
 				if (string.IsNullOrWhiteSpace(jobName))
 					jobName = Forms9Patch.ApplicationInfoService.Name;
 				WebViewPrintHelper printHelper = null;
-				if (webView.Source is HtmlWebViewSource htmlSource && !string.IsNullOrWhiteSpace(htmlSource.Html))
-					printHelper = new WebViewPrintHelper(htmlSource.Html, htmlSource.BaseUrl, jobName);
-				else if (webView.Source is UrlWebViewSource urlSource && !string.IsNullOrWhiteSpace(urlSource.Url))
-					printHelper = new WebViewPrintHelper(urlSource.Url, jobName);
-				else if (Platform.GetRenderer(webView) is WebViewRenderer renderer && renderer.Control is Windows.UI.Xaml.Controls.WebView nativeWebView)
-					printHelper = new WebViewPrintHelper(nativeWebView, jobName);
+				var properties = new Dictionary<string, string>
+							{
+								{ "class", "Forms9Patch.UWP.PrintService" },
+								{ "method", "PrintAsync" },
+							};
+				try
+				{
+					if (webView.Source is HtmlWebViewSource htmlSource && !string.IsNullOrWhiteSpace(htmlSource.Html))
+					{
+						properties["line"] = "47";
+						printHelper = new WebViewPrintHelper(htmlSource.Html, htmlSource.BaseUrl, jobName);
+					}
+					else if (webView.Source is UrlWebViewSource urlSource && !string.IsNullOrWhiteSpace(urlSource.Url))
+					{
+						properties["line"] = "53";
+						printHelper = new WebViewPrintHelper(urlSource.Url, jobName);
+					}
+					else if (Platform.GetRenderer(webView) is WebViewRenderer renderer && renderer.Control is Windows.UI.Xaml.Controls.WebView nativeWebView)
+					{
+						properties["line"] = "57";
+						printHelper = new WebViewPrintHelper(nativeWebView, jobName);
+					}
+				}
+				catch (Exception e)
+				{
+					using (Forms9Patch.Toast.Create("Print Service Error", "Could not initiate print WebViewPrintHelper.  Please try again.\n\nException: " + e.Message + "\n\nInnerException: " + e.InnerException)) { }
+					Analytics.TrackException?.Invoke(e, properties);
+				}
+
 				if (printHelper != null)
 				{
-					printHelper.RegisterForPrinting();
-					await printHelper.Init();
-					bool showprint = await PrintManager.ShowPrintUIAsync();
+					try
+					{
+						properties["line"] = "71";
+						printHelper.RegisterForPrinting();
+						properties["line"] = "73";
+						await printHelper.Init();
+						properties["line"] = "75";
+						bool showprint = await PrintManager.ShowPrintUIAsync();
+					}
+					catch (Exception e)
+                    {
+						using (Forms9Patch.Toast.Create("Print Service Error", "Could not Show Print UI Async.  Please try again.\n\nException: " + e.Message + "\n\nInnerException: " + e.InnerException)) { }
+						Analytics.TrackException?.Invoke(e, properties);
+					}
 				}
 			});
 			return Task.CompletedTask;
